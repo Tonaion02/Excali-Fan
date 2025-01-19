87cf4e728e0518847468eaac8f4c183e27bc5305
Tonaion02 <antonioambrosiopub@gmail.com>
2025-01-19 15:09:56 +0100

    Update: auto update

--- testSignalRInSpring/my-webapp/src/main/resources/static/draw.js
+++ testSignalRInSpring/my-webapp/src/main/resources/static/draw.js
@@ -231,15 +231,15 @@ function draw() {
             }).then(resp => resp.data)
         }
 
-        function deleteLineFromList(lines, indexLineToDelete) {
-            // let indexLineToDelete = -1;
+        function deleteLineFromList(lines, userIdLineToDelete, timestampLineToDelete) {
+            let indexLineToDelete = -1;
             
-            // for(let indexLine in lines) {
-            //     let line = lines[indexLine];
+            for(let indexLine in lines) {
+                let line = lines[indexLine];
 
-            //     if(line.userId == userIdLineToDelete && line.timestamp == timestampLineToDelete)
-            //         indexLineToDelete = indexLine;
-            // }
+                if(line.userId == userIdLineToDelete && line.timestamp == timestampLineToDelete)
+                    indexLineToDelete = indexLine;
+            }
 
             if(indexLineToDelete >= 0)
                 lines.splice(indexLineToDelete, 1);
@@ -295,7 +295,7 @@ function isPointInLine(point, line, tollerance) {
 
 function isPointInLines(point, lines, tollerance) {
 
-    let indexLineToReturn = -1;
+    let lineToReturn = null;
 
     for(indexLine in lines) {
         let line = lines[indexLine];
@@ -305,7 +305,7 @@ function isPointInLines(point, lines, tollerance) {
 
             line.color = "red";
 
-            indexLineToReturn = indexLine;
+            lineToReturn = line;
         }
     }
 
@@ -317,7 +317,7 @@ function isPointInLines(point, lines, tollerance) {
         // T: find a way to delete the currentLine
     }
 
-    return indexLineToReturn;
+    return lineToReturn;
 }
 
 
