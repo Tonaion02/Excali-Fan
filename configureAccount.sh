ee4961e1ad48acaadb6486e08d3f0356508b14aa
Tonaion02 <antonioambrosiopub@gmail.com>
2025-02-23 01:07:30 +0100

    Update of all project

--- configureAccount.sh
+++ configureAccount.sh
@@ -1,4 +1,1 @@
-wget https://downloads.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-mkdir -p $HOME/maven
-tar -xvzf apache-maven-3.9.6-bin.tar.gz -C $HOME/maven
-export PATH=$HOME/maven/apache-maven-3.9.6/bin:$PATH
+wget https://downloads.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz & mkdir -p $HOME/maven & tar -xvzf apache-maven-3.9.6-bin.tar.gz -C $HOME/maven & export PATH=$HOME/maven/apache-maven-3.9.6/bin:$PATH & read -p "Press enter to continue"
