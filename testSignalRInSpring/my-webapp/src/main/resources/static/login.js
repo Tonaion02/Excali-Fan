53879abc1f9ef593be7e83f8fe9129e3b463698c
Tonaion02 <antonioambrosiopub@gmail.com>
2025-02-23 02:09:08 +0100

    Update: auto update

--- testSignalRInSpring/my-webapp/src/main/resources/static/login.js
+++ testSignalRInSpring/my-webapp/src/main/resources/static/login.js
@@ -1,3 +1,29 @@
+import * as msal from "@azure/msal-browser";
+
+const msalConfig = {
+  auth: {
+    clientId: "c751136e-e609-4363-ab87-32206c7674ec",
+    authority: "https://login.microsoftonline.com/d5364e8d-9d78-49f0-9636-be777bbe9507",
+    redirectUri: "https://rest-service-1735827345127.azurewebsites.net/api/testEntraId",
+  },
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+async function login() {
+  const loginRequest = {
+    scopes: ["openid", "profile", "email"],
+  };
+
+  try {
+    const loginResponse = await msalInstance.loginPopup(loginRequest);
+    console.log("Access Token:", loginResponse.accessToken);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 export function login() {
     console.log("Successfull login");
 }
