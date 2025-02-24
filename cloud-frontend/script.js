7b3bb5429857909fdbc66609c9515dedbaac9546
Tonaion02 <antonioambrosiopub@gmail.com>
2025-02-24 10:49:17 +0100

    Update: auto update

---
+++ cloud-frontend/script.js
@@ -0,0 +1,81 @@
+const canvas = document.getElementById('drawingCanvas');
+const context = canvas.getContext('2d');
+
+// Gestione del colore di sfondo al clic
+const pencilContainer = document.getElementById('pencil-container');
+const eraserContainer = document.getElementById('eraser-container');
+const colorDropdown = document.getElementById('color-dropdown');
+
+const colorOptions = [
+    { color: '#ffffff', element: document.createElement('div') },
+    { color: '#ff69b4', element: document.createElement('div') },
+    { color: '#006400', element: document.createElement('div') },
+    { color: '#00008b', element: document.createElement('div') }
+];
+
+colorOptions.forEach(option => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('color-wrapper');
+    option.element.classList.add('color-option');
+    option.element.style.backgroundColor = option.color;
+    option.element.setAttribute('data-color', option.color);
+    wrapper.appendChild(option.element);
+    colorDropdown.appendChild(wrapper);
+
+    option.element.addEventListener('click', () => {
+        context.strokeStyle = option.color;
+        document.querySelectorAll('.color-wrapper').forEach(wrap => wrap.classList.remove('selected'));
+        wrapper.classList.add('selected');
+        colorDropdown.style.display = 'none';
+    });
+});
+
+// Imposta il colore bianco come colore selezionato di base
+document.querySelector('.color-option[data-color="#ffffff"]').parentElement.classList.add('selected');
+context.strokeStyle = '#ffffff';
+
+pencilContainer.addEventListener('click', () => {
+    pencilContainer.style.backgroundColor = '#403e6a'; // Colore viola più scuro
+    eraserContainer.style.removeProperty('background-color');
+    colorDropdown.style.display = colorDropdown.style.display === 'flex' ? 'none' : 'flex';
+});
+
+
+eraserContainer.addEventListener('click', () => {
+    eraserContainer.style.backgroundColor = '#403e6a'; // Colore viola più scuro
+    pencilContainer.style.removeProperty('background-color');
+    colorDropdown.style.display = 'none';
+});
+
+// Nascondi la tendina dei colori all'inizio
+colorDropdown.style.display = 'none';
+
+// Gestione del menu
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+const exportImageButton = document.getElementById('export-image');
+const saveButton = document.getElementById('save');
+
+menuButton.addEventListener('click', () => {
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+exportImageButton.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.download = 'disegno.png';
+    link.href = canvas.toDataURL();
+    link.click();
+});
+
+saveButton.addEventListener('click', () => {
+    // Aggiungi qui il codice per salvare il disegno
+    alert('Funzione di salvataggio non implementata');
+});
+
+// Gestione delle dimensioni del canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
---
+++ testSignalRInSpring/my-webapp/src/main/java/com/example/restservice/TokenValidatorEntraId.java
@@ -0,0 +1,44 @@
+package com.example.restservice;
+
+import java.security.interfaces.RSAPublicKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
+import com.auth0.jwk.*;
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkProvider;
+
+import java.net.URL;
+
+
+
+
+
+public class TokenValidatorEntraId {
+    private static final String JWKS_URL = "https://login.microsoftonline.com/common/discovery/v2.0/keys";
+    private static final String EXPECTED_AUDIENCE = "b1453203-8719-4a2a-8cc6-96bf883a7e65";
+    
+    public static boolean validateToken(String token) {
+        try {
+            JwkProvider provider = new UrlJwkProvider(new URL(JWKS_URL));
+            String kid = Jwts.parser().parseClaimsJws(token).getHeader().getKeyId();
+            Jwk jwk = provider.get(kid);
+            
+            RSAPublicKey publicKey = (RSAPublicKey) jwk.getPublicKey();
+            
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(publicKey)
+                    .requireAudience(EXPECTED_AUDIENCE)
+                    .requireIssuer("https://login.microsoftonline.com/common/v2.0")
+                    .parseClaimsJws(token);
+            
+            System.out.println("Token valido: " + claims.getBody());
+            return true;
+        } catch (Exception e) {
+            System.err.println("Token non valido: " + e.getMessage());
+            return false;
+        }
+    }
+}
