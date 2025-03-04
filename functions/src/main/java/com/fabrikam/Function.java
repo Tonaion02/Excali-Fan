50c75371df788e9955edadc35f9ae39b45e90512
Tonaion02 <antonioambrosiopub@gmail.com>
2025-03-04 15:10:22 +0100

    Update: auto update

--- functions/src/main/java/com/fabrikam/Function.java
+++ functions/src/main/java/com/fabrikam/Function.java
@@ -24,29 +24,29 @@ public class Function {
     public Function() {
         this.secret = "gervaso";
 
-        // try {
-        //     SecretClient secretClient = null;
-        //     if(keySignalR == null || accountKeyBlobStorage == null) {
-        //         secretClient = new SecretClientBuilder()
-        //         .vaultUrl(keyVaultUrl)
-        //         .credential(new DefaultAzureCredentialBuilder().build())
-        //         .buildClient();
-        //     }
+        try {
+            SecretClient secretClient = null;
+            if(keySignalR == null || accountKeyBlobStorage == null) {
+                secretClient = new SecretClientBuilder()
+                .vaultUrl(keyVaultUrl)
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+            }
     
-        //     if(keySignalR == null) {
-        //         String secretValueForSignalR = secretClient.getSecret(secretNameKeySignalR).getValue();
-        //         keySignalR = secretValueForSignalR;
-        //     }
+            if(keySignalR == null) {
+                String secretValueForSignalR = secretClient.getSecret(secretNameKeySignalR).getValue();
+                keySignalR = secretValueForSignalR;
+            }
 
-        //     // if(accountKeyBlobStorage == null) {
-        //     //     String secretValueForAzureBlobStorage = secretClient.getSecret(secretNameBlobStorageAccount).getValue();
-        //     //     accountKeyBlobStorage = secretValueForAzureBlobStorage;
-        //     // }
-        // } catch(RuntimeException e) {
-        //     e.printStackTrace();
-        // } catch(Exception e) {
-        //     e.printStackTrace();
-        // }
+            // if(accountKeyBlobStorage == null) {
+            //     String secretValueForAzureBlobStorage = secretClient.getSecret(secretNameBlobStorageAccount).getValue();
+            //     accountKeyBlobStorage = secretValueForAzureBlobStorage;
+            // }
+        } catch(RuntimeException e) {
+            e.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
