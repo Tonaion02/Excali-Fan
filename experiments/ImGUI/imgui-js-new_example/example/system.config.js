926d362dd8d5db34f795fb5d4eb9941f0ed4f167
Tonaion02 <antonioambrosiopub@gmail.com>
2025-06-28 19:54:11 +0200

    EXPERIMENTS: experimenting with a Javascript version of ImGUI
    Trying to understand the structure of the code and developing a little ImGUI interface

---
+++ experiments/ImGUI/imgui-js-new_example/example/system.config.js
@@ -0,0 +1,10 @@
+System.config({
+    map: {
+        imports: {
+            "new_example": "./build/new_example.js",
+            "bind-imgui": "../build/bind-imgui.js",
+            "imgui-js": "../build/imgui.js",
+            "main": "./build/main.js",
+        }
+    },
+});
