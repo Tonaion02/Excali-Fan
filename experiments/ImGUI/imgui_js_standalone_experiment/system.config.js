ae3fbe6b148a08f89c722403a145dc2ba22d85fd
Tonaion02 <antonioambrosiopub@gmail.com>
2025-07-05 06:19:03 +0200

    EXPERIMENTS: experimenting with a Javascript version of ImGUI
    The directory structure is now simpler

---
+++ experiments/ImGUI/imgui_js_standalone_experiment/system.config.js
@@ -0,0 +1,11 @@
+System.config({
+    map: {
+        imports: {
+            "new_example": "./ImGUI/new_example.js",
+            "bind-imgui": "./ImGUI/core/bind-imgui.js",
+            "imgui-js": "./ImGUI/core/imgui.js",
+            
+            "main": "./ImGUI/main.js",
+        }
+    },
+});
