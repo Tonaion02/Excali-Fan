adcdc9c6f30cf0fca3b887bbb7d1a1b7b5e978d6
Tonaion02 <antonioambrosiopub@gmail.com>
2026-03-07 23:36:00 +0100

    Update commit

--- webapp/src/main/java/com/example/restservice/serviceSignalR/GarbageBoardCollector.java
+++ webapp/src/main/java/com/example/restservice/serviceSignalR/GarbageBoardCollector.java
@@ -6,11 +6,17 @@ import com.example.restservice.serviceSignalR.*;
 @Component
 public class GarbageBoardCollector {
 
+    private final SignalRContoller contoller;
+
+    public GarbageBoardCollector(SignalRContoller contoller) {
+        this.contoller = controller;
+    }
+
     // T: This method runs each 5 seconds to collect the boards that are garbage
     @Scheduled(fixedDelay = 20000)
     public void garbage_collect() {
         System.out.println("Running GarbageBoardCollector at: " + System.currentTimeMillis());
 
-        System.out.println("boards from GarbageBoardCollector point of view: " + SignalRController.boards);
+        System.out.println("boards from GarbageBoardCollector point of view: " + contoller.boards);
     }
 }
