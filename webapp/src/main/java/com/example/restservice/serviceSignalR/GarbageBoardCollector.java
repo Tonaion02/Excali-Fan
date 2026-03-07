58bf8ecfe794eb5912a5be170465ff6c55480a0a
Tonaion02 <antonioambrosiopub@gmail.com>
2026-03-07 23:28:40 +0100

    Update commit

---
+++ webapp/src/main/java/com/example/restservice/serviceSignalR/GarbageBoardCollector.java
@@ -0,0 +1,14 @@
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GarbageBoardCollector {
+
+    // T: This method runs each 5 seconds to collect the boards that are garbage
+    @Scheduled(fixedDelay = 20000)
+    public void garbage_collect() {
+        System.out.println("Running GarbageBoardCollector at: " + System.currentTimeMillis());
+
+        System.out.println("boards from GarbageBoardCollector point of view: " + SignalRController.boards);
+    }
+}
