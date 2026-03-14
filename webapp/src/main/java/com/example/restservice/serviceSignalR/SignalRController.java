a44e8556ac0eabd27ccacd1e84a88f4da66603cc
Tonaion02 <antonioambrosiopub@gmail.com>
2026-03-14 18:07:34 +0100

    update commit

--- webapp/src/main/java/com/example/restservice/serviceSignalR/SignalRController.java
+++ webapp/src/main/java/com/example/restservice/serviceSignalR/SignalRController.java
@@ -345,128 +345,128 @@ public class SignalRController {
 
 
 
-    public static class RequestBodyBlobToLoad {
-        public RequestBodyBlobToLoad(String blobName, String userId) {
-            this.blobName = blobName;
-            this.userId = userId;
-        }
+    // public static class RequestBodyBlobToLoad {
+    //     public RequestBodyBlobToLoad(String blobName, String userId) {
+    //         this.blobName = blobName;
+    //         this.userId = userId;
+    //     }
 
-        public String getBlobName() {
-            return blobName;
-        }
+    //     public String getBlobName() {
+    //         return blobName;
+    //     }
 
-        public void setBlobName(String blobName) {
-            this.blobName = blobName;
-        }
+    //     public void setBlobName(String blobName) {
+    //         this.blobName = blobName;
+    //     }
 
-        public void setUserId(String userId) {
-            this.userId = userId;
-        }
+    //     public void setUserId(String userId) {
+    //         this.userId = userId;
+    //     }
 
-        public String getUserId() {
-            return userId;
-        }
+    //     public String getUserId() {
+    //         return userId;
+    //     }
 
-        private String blobName;
-        private String userId;
-    }
+    //     private String blobName;
+    //     private String userId;
+    // }
 
-    public static class LoadBoardResult {
-        public LoadBoardResult(String boardSessionId, String boardJson) {
-            this.boardSessionId = boardSessionId;
-            this.boardJson = boardJson;
-        }
+    // public static class LoadBoardResult {
+    //     public LoadBoardResult(String boardSessionId, String boardJson) {
+    //         this.boardSessionId = boardSessionId;
+    //         this.boardJson = boardJson;
+    //     }
 
-        public String boardSessionId;
-        public String boardJson;
-    }
+    //     public String boardSessionId;
+    //     public String boardJson;
+    // }
 
-    // T: This private api is used to load the Blob of a Board
-    // identified by its name. The api return the board formatted
-    // like a json and then load it in the "remote boards"(boards stored 
-    // in central memory of the Server).
-    // T: WARNING remember to return the new BoardSessionId or find
-    // another solution
-    @PostMapping("/api/loadBoard")
-    public LoadBoardResult loadBoard(@RequestHeader("Authorization") String accessToken, @RequestBody RequestBodyBlobToLoad requestBody) {
+    // // T: This private api is used to load the Blob of a Board
+    // // identified by its name. The api return the board formatted
+    // // like a json and then load it in the "remote boards"(boards stored 
+    // // in central memory of the Server).
+    // // T: WARNING remember to return the new BoardSessionId or find
+    // // another solution
+    // @PostMapping("/api/loadBoard")
+    // public LoadBoardResult loadBoard(@RequestHeader("Authorization") String accessToken, @RequestBody RequestBodyBlobToLoad requestBody) {
         
-        // T: Retrieve email from token (START)
-        String email = null;
-        try {
-            SignedJWT signedJwt = SignedJWT.parse(accessToken);
-            email = signedJwt.getJWTClaimsSet().getStringClaim("email");
-        } catch(Exception e) {
-            System.out.println("signedJwt exception: " + e.getMessage());
-            e.printStackTrace();
-        }
-        if(email == null) {
-            System.out.println("email retrieved is null");
-            return null;
-        }
-        System.out.println("email of user retrieved from token: " + email);
-        // T: Retrieve email from token (END)
+    //     // T: Retrieve email from token (START)
+    //     String email = null;
+    //     try {
+    //         SignedJWT signedJwt = SignedJWT.parse(accessToken);
+    //         email = signedJwt.getJWTClaimsSet().getStringClaim("email");
+    //     } catch(Exception e) {
+    //         System.out.println("signedJwt exception: " + e.getMessage());
+    //         e.printStackTrace();
+    //     }
+    //     if(email == null) {
+    //         System.out.println("email retrieved is null");
+    //         return null;
+    //     }
+    //     System.out.println("email of user retrieved from token: " + email);
+    //     // T: Retrieve email from token (END)
         
         
         
-        String boardJson = null;
+    //     String boardJson = null;
         
-        BoardStorage boardStorage = new BoardStorage();
-        boardJson = boardStorage.loadBoard(requestBody.blobName, email);
+    //     BoardStorage boardStorage = new BoardStorage();
+    //     boardJson = boardStorage.loadBoard(requestBody.blobName, email);
 
 
 
-        // T: WARNING: you can substitute that with UserId(email) + timestamp
-        // int numericBoardSessionId = Math.abs(ThreadLocalRandom.current().nextInt());
-        // String boardSessionId = Integer.toString(numericBoardSessionId);
-        String boardSessionId = BoardIdGenerator.generateIdBoard(requestBody.userId);
+    //     // T: WARNING: you can substitute that with UserId(email) + timestamp
+    //     // int numericBoardSessionId = Math.abs(ThreadLocalRandom.current().nextInt());
+    //     // String boardSessionId = Integer.toString(numericBoardSessionId);
+    //     String boardSessionId = BoardIdGenerator.generateIdBoard(requestBody.userId);
 
 
 
-        // T: autojoin the new group (START)
-        System.out.println("adding to group");
+    //     // T: autojoin the new group (START)
+    //     System.out.println("adding to group");
 
-        String hubUrl = Keys.signalRServiceBaseEndpoint + "/api/v1/hubs/" + hubName + "/groups/" + boardSessionId + "/users/" + requestBody.userId;
-        String accessKey = generateJwt(hubUrl, email);
+    //     String hubUrl = Keys.signalRServiceBaseEndpoint + "/api/v1/hubs/" + hubName + "/groups/" + boardSessionId + "/users/" + requestBody.userId;
+    //     String accessKey = generateJwt(hubUrl, email);
 
-        HttpResponse<String> response = Unirest.put(hubUrl)
-            .header("Content-Type", "application/json")
-            .header("Authorization", "Bearer " + accessKey)
-            .asString();
+    //     HttpResponse<String> response = Unirest.put(hubUrl)
+    //         .header("Content-Type", "application/json")
+    //         .header("Authorization", "Bearer " + accessKey)
+    //         .asString();
 
-        System.out.println("addgroup: " + response.getStatus());
-        System.out.println("addgroup: " + response.getBody());        
-        // T: autojoin the new group (END)
+    //     System.out.println("addgroup: " + response.getStatus());
+    //     System.out.println("addgroup: " + response.getBody());        
+    //     // T: autojoin the new group (END)
 
 
 
-        // T: parse the board in a board object from json (START)
-        Board board = null;
-        board = boardStorage.parseBoardFromJson(boardJson);
+    //     // T: parse the board in a board object from json (START)
+    //     Board board = null;
+    //     board = boardStorage.parseBoardFromJson(boardJson);
 
-        if(board == null)
-        {
-            System.out.println("FAILED TO LOAD BOARD");
-            return new LoadBoardResult(boardSessionId, boardJson);
-        }
-        // T: parse the board in a board object from json (END)
-
-        
-
-        // T: Set the right hostUserId
-        board.setHostUserId(requestBody.userId);
-
-        // T: Set the last instant of modification
-        board.setInstantLastMod(Instant.now());
-
-        // T: Add board to the collection of boards
-        boards.boards.put(boardSessionId, board);
+    //     if(board == null)
+    //     {
+    //         System.out.println("FAILED TO LOAD BOARD");
+    //         return new LoadBoardResult(boardSessionId, boardJson);
+    //     }
+    //     // T: parse the board in a board object from json (END)
 
         
-        System.out.println("Succsefully added board in the server");
+
+    //     // T: Set the right hostUserId
+    //     board.setHostUserId(requestBody.userId);
+
+    //     // T: Set the last instant of modification
+    //     board.setInstantLastMod(Instant.now());
+
+    //     // T: Add board to the collection of boards
+    //     boards.boards.put(boardSessionId, board);
+
+        
+    //     System.out.println("Succsefully added board in the server");
 
 
-        return new LoadBoardResult(boardSessionId, boardJson);
-    }
+    //     return new LoadBoardResult(boardSessionId, boardJson);
+    // }
 
     public static class RequestBodyBlobToSave {
         public RequestBodyBlobToSave(String blobName, String email, String boardSessionId, String precBoardStorageId) {
